78a79c804b4a291e6cf7c441dfb38285f56b4552
AlexHCJP <alexganbert@gmail.com>
2023-03-15 23:07:37 +0700

    -my_pef

--- lib/logic/screens/my_perf/details/my_performers_details_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'my_performers_details_state.dart';
-
-class MyPerformersDetailsCubit extends Cubit<MyPerformersDetailsState> {
-  MyPerformersDetailsCubit() : super(MyPerformersDetailsInitial());
-}
--- lib/logic/screens/my_perf/main/my_performers_main_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'my_performers_main_state.dart';
-
-class MyPerformersMainCubit extends Cubit<MyPerformersMainState> {
-  MyPerformersMainCubit() : super(MyPerformersMainInitial());
-}
